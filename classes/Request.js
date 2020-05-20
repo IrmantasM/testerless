6a3357a2f7a214329eb95b031b25e56b820e5a32
Irmantas Marozas <irmantas.t.marozas@accenture.com>
2020-05-20 21:39:01 +0100

    Add HTTP call function

--- classes/Request.js
+++ classes/Request.js
@@ -1,8 +1,10 @@
 class Request {
-  constructor(type, payload, path) {
-    this.type    = type;
-    this.payload = payload;
-    this.path    = path;
+  constructor(type, payload, headers, hostname, path) {
+    this.type     = type;
+    this.payload  = payload;
+    this.headers  = headers || ''; // Optional
+    this.hostname = hostname || ''; // Optional
+    this.path     = path || ''; // Optional
   }
 }
 
